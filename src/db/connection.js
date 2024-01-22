b9ef9f31611493326a96c63f13b2047de3a88cc6
0abdullahansari <chittij7@gmail.com>
2024-01-22 15:26:50 +0530

    Refine

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,7 @@
 import mysql2 from "mysql2/promise";
 
+// For using hosted database.
+
 const pool = mysql2.createPool({
   host: "database-theattice.a.aivencloud.com",
   user: "avnadmin",
@@ -10,6 +12,18 @@ const pool = mysql2.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+// For using local database.
+
+// const pool = mysql2.createPool({
+//   host: "localhost",
+//   user: "your username",
+//   password: "your password",
+//   database: "defaultdb",   ---> Change if needed
+//   connectionLimit: 10,
+//   port: 3306,      ----------->Change if needed
+//   waitForConnections: true,
+// });
 
 // This function returns a MySQL connection from pool.
 
